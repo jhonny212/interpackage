9202a99cdbe12c35f8a62c0c7f66d9b0a6c0228f
jmateo95 <201313852@ingenieria.usac.edu.gt>
2023-04-16 14:26:10 -0600

    feat: roles

--- InterPackage-Tracking/src/main/java/com/interpackage/tracking/controller/Hola.java
+++ InterPackage-Tracking/src/main/java/com/interpackage/tracking/controller/Hola.java
@@ -1,5 +1,8 @@
 package com.interpackage.tracking.controller;
-
+//import com.interpackage.tracking.aspect.RequiredPermission;
+import com.interpackage.basedomains.aspect.RequiredRole;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,13 +11,17 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+
 @RestController
-@RequestMapping ("/api/tracking/v1")
+@RequestMapping("/api/tracking/v1")
 public class Hola {
+    @Autowired
+    private HttpServletRequest request;
+
 
     @GetMapping("/hola/{name}")
+    @RequiredRole({"Admin"})
     public ResponseEntity<Object> getHola(@PathVariable String name) {
-
         return ResponseEntity.ok(
                 new HashMap<String, Object>() {{
                     put("mensaje", "Hola " + name);
@@ -22,8 +29,20 @@ public class Hola {
                 }}
         );
     }
+
     @GetMapping("/adios/{name}")
-    public ResponseEntity<String> getAdios(@PathVariable String name) {
-        return ResponseEntity.ok("Adios " + name);
+    @RequiredRole({"User"})
+    public ResponseEntity<Object> getAdios(@PathVariable String name) {
+
+        String permissionsHeader = request.getHeader("permissions");
+        List<String> permissions = Arrays.asList(permissionsHeader.split(","));
+        System.out.println((permissions));
+
+        return ResponseEntity.ok(
+                new HashMap<String, Object>() {{
+                    put("mensaje", "Adios " + name);
+                    put("estado", HttpStatus.OK.value());
+                }}
+        );
     }
 }
