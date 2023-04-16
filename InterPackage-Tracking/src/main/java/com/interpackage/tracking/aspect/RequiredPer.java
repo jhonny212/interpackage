8e9aabeed37393fea064c558c75719d4b8e863e3
jmateo95 <201313852@ingenieria.usac.edu.gt>
2023-04-16 14:26:50 -0600

    fix: doble modulo

--- InterPackage-Tracking/src/main/java/com/interpackage/tracking/aspect/RequiredPer.java
+++
@@ -1,9 +0,0 @@
-package com.interpackage.tracking.aspect;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface RequiredPer {
-    String[] value();
-}
